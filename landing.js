86270b0a51f6dcd0566edb1b31d5b2089ede2ec7
tresenish <korolvolodymyr0@gmail.com>
2024-03-22 21:20:39 -0500

    minor fixes  + blue headings text update

--- landing.js
+++ landing.js
@@ -1,3 +1,4 @@
+// SCROLLER
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
@@ -17,18 +18,16 @@ function addAnimation() {
   });
 }
 
-
+// NAV-Bar for phone
 document.addEventListener('DOMContentLoaded', function() {
     const dropdownButton = document.getElementById('dropdown-button');
     const navMenu = document.getElementById('nav-ul-id');
-
-    // Toggle the 'is-visible' class on click
     dropdownButton.addEventListener('click', function() {
         navMenu.classList.toggle('is-visible');
     });
 });
 
-// Transform-animation
+//Rotate POLYGONS
 window.addEventListener('scroll', function() {
   const polygons = document.querySelectorAll('.pol.section3x');
   const scrollTop = document.documentElement.scrollTop;
@@ -40,3 +39,58 @@ window.addEventListener('scroll', function() {
     polygon.style.transform = `translate(-0%,-50%) rotate(${rotation}deg)`;
   });
 });
+
+
+
+
+const pElement = document.getElementById('pBlue');
+const anchorElements = document.querySelectorAll('.blueHeadings a');
+
+function updateText(text) {
+  pElement.innerHTML = text;
+}
+
+function updateActiveClass(clickedId) {
+  anchorElements.forEach(anchor => {
+    if (anchor.id === clickedId) {
+      anchor.classList.add('active');
+    } else {
+      anchor.classList.remove('active');
+    }
+  });
+}
+
+anchorElements.forEach(anchor => {
+  anchor.addEventListener('click', function() {
+    switch(this.id) {
+      case 'startupsBlue':
+        updateText('Empower startups to rapidly build, iterate, and refine products with a focus on user-centered design, enabling them to achieve product-market fit, gain traction, and access early-stage investments.<br><br>We provide startups with the necessary infrastructure, resources,and expertise to accelerate their growth and secure funding.');
+        break;
+      case 'investorsBlue':
+        updateText('We use low-code and no-code tools for application design & development to help investors to reduce development costs,validate ideas with real-world market feedback, and accelerate the growth of their portfolio companies.');
+        break;
+      case 'sbmsBlue':
+        updateText('We design & develop applications that help SMBs optimize operations, automate tasks, and build client-centred solutions that lead to increased revenue and cost savings. We help SMBs to adapt quickly to changing market demands and scale their operations effectively, positioning them for long-term growth and success.');
+        break;
+      case 'agenciesBlue':
+        updateText('Helping agencies to deliver innovative solutions to clients faster, more efficiently, and at a lower cost. We use the most modern development platforms to provide agencies with the abilities to build applications, automate workflows for their clients\' growth.');
+        break;
+    }
+    updateActiveClass(this.id);
+  });
+});
+
+
+
+  // const pElement = document.getElementById('pBlue');
+  // function updateText(text) {
+  //   pElement.innerHTML = text;
+  // }
+  // document.getElementById('startupsBlue').addEventListener('click', function() {
+  // });
+  // document.getElementById('investorsBlue').addEventListener('click', function() {
+  // });
+  // document.getElementById('sbmsBlue').addEventListener('click', function() {
+  // });
+  // document.getElementById('agenciesBlue').addEventListener('click', function() {
+  // });
